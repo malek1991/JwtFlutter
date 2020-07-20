ac6bea99deb28ac4fbbc6024a20926dfb249370f
malek.slimani <malek.slimani@vistex.com>
2020-07-20 14:58:06 +0200

    First Commit

---
+++ lib/network/remote_data_source.dart
@@ -0,0 +1,155 @@
+/* 
+ * Copyright (c) 2020 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ * 
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:book_app/network/book_client.dart';
+import 'package:book_app/ui/favorite_book_screen/favorite_book_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+import '../model/library.dart';
+import '../model/network_response.dart';
+import '../model/result.dart';
+import '../util/request_type.dart';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class RemoteDataSource {
+  //Creating Singleton
+  RemoteDataSource._privateConstructor();
+
+  static final RemoteDataSource _apiResponse =
+  RemoteDataSource._privateConstructor();
+
+  factory RemoteDataSource() => _apiResponse;
+
+  BookClient client = BookClient(Client());
+
+  StreamController<Result> _addBookStream;
+
+  final _storage = FlutterSecureStorage();
+
+  Stream<Result> hasBookAdded() => _addBookStream.stream;
+
+  Future<Result> signIn(String username, String password,
+      BuildContext context) async {
+    try {
+      Map data = {
+        'username': 'malekazher.slimani@gmail.com',
+        'password': 'Test@123'
+      };
+      final response = await client.request(
+          requestType: RequestType.POST,
+          path: "api/Auth/Login",
+          parameter: json.encode(data));
+      if (response.statusCode == 200) {
+        var body = json.decode(response.body);
+        await _storage.write(key: "token", value: body['token']);
+        await _storage.write(key: "expireDate", value: body['expireDate']);
+        if (body['token'] != null) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => FavoriteBooksScreen()),
+          );
+        }
+        return Result.success('Success');
+      } else {
+        return Result.error("User not Auth");
+      }
+    } catch (error) {
+      return Result.error("Something went wrong!");
+    }
+  }
+
+  void init() => _addBookStream = StreamController();
+
+  Future<Result> getBooks() async {
+    try {
+      final response = await client.request(
+          requestType: RequestType.GET, path: "api/Products");
+      if (response.statusCode == 200) {
+        Iterable list = json.decode(response.body)['data'];
+        return Result<Library>.success(Library.fromJson(list));
+      } else {
+        return Result.error("Book list not available");
+      }
+    } catch (error) {
+      return Result.error("Something went wrong!");
+    }
+  }
+
+  void addBook(String _name, String _unit, int _price) async {
+    _addBookStream.sink.add(Result<String>.loading("Loading"));
+    try {
+      Map data = {
+        'name': _name,
+        'unit': _unit,
+        'price': _price
+      };
+
+      final response = await client.request(
+          requestType: RequestType.POST,
+          path: "api/products/",
+          parameter: json.encode(data));
+      if (response.statusCode == 200) {
+        _addBookStream.sink.add(Result<NetworkResponse>.success(
+            NetworkResponse.fromRawJson(response.body)));
+      } else {
+        _addBookStream.sink.add(Result.error("Something went wrong"));
+      }
+    } catch (error) {
+      _addBookStream.sink.add(Result.error("Something went wrong!"));
+    }
+  }
+
+  Future<Result> deleteBook(int index) async {
+    try {
+      final response = await client.request(
+          requestType: RequestType.DELETE, path: "deleteBook/$index");
+      if (response.statusCode == 200) {
+        return Result<NetworkResponse>.success(
+            NetworkResponse.fromRawJson(response.body));
+      } else {
+        return Result<NetworkResponse>.error(
+            NetworkResponse.fromRawJson(response.body));
+      }
+    } catch (error) {
+      return Result.error("Something went wrong!");
+    }
+  }
+
+  void dispose() => _addBookStream.close();
+}
---
+++ lib/ui/loginscreen/loginscreen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:book_app/network/remote_data_source.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _isLoading = false;
+
+  RemoteDataSource _apiResponse = RemoteDataSource();
+
+  @override
+  void initState() {
+    super.initState();
+    _apiResponse.init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light
+        .copyWith(statusBarColor: Colors.transparent));
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.blue, Colors.teal],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter),
+        ),
+        child: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView(
+                children: <Widget>[
+                  headerSection(),
+                  textSection(),
+                  buttonSection(),
+                ],
+              ),
+      ),
+    );
+  }
+
+  Container buttonSection() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 40.0,
+      padding: EdgeInsets.symmetric(horizontal: 15.0),
+      margin: EdgeInsets.only(top: 15.0),
+      child: RaisedButton(
+        onPressed: emailController.text == "" || passwordController.text == ""
+            ? null
+            : () {
+                setState(() {
+                  _isLoading = true;
+                });
+                _apiResponse.signIn(
+                    emailController.text, passwordController.text, context);
+              },
+        elevation: 0.0,
+        color: Colors.purple,
+        child: Text("Sign In", style: TextStyle(color: Colors.white70)),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+      ),
+    );
+  }
+
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+
+  Container textSection() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            controller: emailController,
+            cursorColor: Colors.white,
+            style: TextStyle(color: Colors.white70),
+            decoration: InputDecoration(
+              icon: Icon(Icons.email, color: Colors.white70),
+              hintText: "Email",
+              border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white70)),
+              hintStyle: TextStyle(color: Colors.white70),
+            ),
+          ),
+          SizedBox(height: 30.0),
+          TextFormField(
+            controller: passwordController,
+            cursorColor: Colors.white,
+            obscureText: true,
+            style: TextStyle(color: Colors.white70),
+            decoration: InputDecoration(
+              icon: Icon(Icons.lock, color: Colors.white70),
+              hintText: "Password",
+              border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white70)),
+              hintStyle: TextStyle(color: Colors.white70),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container headerSection() {
+    return Container(
+      margin: EdgeInsets.only(top: 50.0),
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+      child: Text("My Library Portal",
+          style: TextStyle(
+              color: Colors.white70,
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold)),
+    );
+  }
+}
